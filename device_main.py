8fdc9061fcfffcf2ec92865332dd0229d35a2af3
non <pox2n@icloud.com>
2021-07-11 14:11:46 +0900

    ｓ

--- device_main.py
+++ device_main.py
@@ -58,6 +58,43 @@ logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 logging.basicConfig()
 
+##モニター初期処理################################################################
+# Define the Reset Pin
+oled_reset = digitalio.DigitalInOut(board.D4)
+
+# モニターサイズ（128×64）
+WIDTH = 128
+HEIGHT = 64
+BORDER = 2
+
+# Use for I2C.
+i2c = board.I2C()
+oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C, reset=oled_reset)
+
+# Clear display.
+oled.fill(0)
+oled.show()
+
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+image = Image.new("1", (oled.width, oled.height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a white background
+draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
+
+# Draw a smaller inner rectangle
+draw.rectangle(
+    (BORDER, BORDER, oled.width - BORDER - 1, oled.height - BORDER - 1),
+    outline=0,
+    fill=0,
+)
+
+########################################################################################
+
+
 # 引数のチェックと取得、設定
 def arg_check():
     """
@@ -438,39 +475,3 @@ if __name__ == "__main__":
     device_main()
 
 
-##モニター初期処理################################################################
-# Define the Reset Pin
-oled_reset = digitalio.DigitalInOut(board.D4)
-
-# モニターサイズ（128×64）
-WIDTH = 128
-HEIGHT = 64
-BORDER = 2
-
-# Use for I2C.
-i2c = board.I2C()
-oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3C, reset=oled_reset)
-
-# Clear display.
-oled.fill(0)
-oled.show()
-
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-image = Image.new("1", (oled.width, oled.height))
-
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-
-# Draw a white background
-draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
-
-# Draw a smaller inner rectangle
-draw.rectangle(
-    (BORDER, BORDER, oled.width - BORDER - 1, oled.height - BORDER - 1),
-    outline=0,
-    fill=0,
-)
-
-
-########################################################################################
